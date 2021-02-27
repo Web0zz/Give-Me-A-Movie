6aa093f3546723d2388232d76aaaae70891c179c
Furkan_webD <furkan.ozen4@ogr.sakarya.edu.tr>
2021-02-27 03:15:37 +0300

    fixed

--- app/src/main/java/com/example/givemeamovie/dto/movie_lists/Movie_list.kt
+++ app/src/main/java/com/example/givemeamovie/dto/movie_lists/Movie_list.kt
@@ -1,3 +1,8 @@
 package com.example.givemeamovie.dto.movie_lists
 
-data class Movie_list()
+data class Movie_list(
+    val page: Int,
+    val results: List<Movie>,
+    val total_pages: Int,
+    val total_results: Int
+)
