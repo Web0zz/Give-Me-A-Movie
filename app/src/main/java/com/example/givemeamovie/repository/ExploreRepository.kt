3d71fc950c4918fed9b40a03c7f1dfa279dc9246
Furkan_webD <furkan.ozen4@ogr.sakarya.edu.tr>
2021-03-08 02:48:44 +0300

    repository reImplemented

--- app/src/main/java/com/example/givemeamovie/repository/ExploreRepository.kt
+++ app/src/main/java/com/example/givemeamovie/repository/ExploreRepository.kt
@@ -7,6 +7,9 @@ import com.example.givemeamovie.data.remote.service.MovieListService
 import com.example.givemeamovie.model.entity.Movie
 import com.example.givemeamovie.model.entity.MovieLibraryCrossRef
 import com.example.givemeamovie.model.network.movie_lists.Movie_list
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import javax.inject.Inject
 
 class ExploreRepository @Inject constructor(
@@ -15,11 +18,95 @@ class ExploreRepository @Inject constructor(
         private val croffRefDao: MovieLibraryWithMoviesDao,
 ): Repository {
 
+    companion object {
+        var currentPage = 1
+    }
+
+    private var temporaryList = mutableListOf<Resource<Movie_list>>()
+
+    // Get Movies in Liked database
+    private suspend fun moviesInLikedCross(
+            onSuccess:  suspend (List<MovieLibraryCrossRef>) -> Unit,
+            onError: () -> Unit
+    ) = withContext(Dispatchers.IO) {
+        try {
+            val movieList = croffRefDao.getMovies("LIKED")
+            if(movieList != emptyList<MovieLibraryCrossRef>()) {
+                onSuccess(movieList)
+            } else {
+                onError()
+            }
+        } catch (e: Throwable) {
+            onError()
+        }
+    }
+
+   /* Get popular movie from api when there is no liked movie
+   *  also when get liked movie operation had failed
+   *  */
+    private suspend fun getMovieFromPopular(
+           onSuccess: suspend (Resource<Movie_list>) -> Unit,
+           onError: (Throwable) -> Unit
+    ) = withContext(Dispatchers.IO) {
+        try {
+            if(temporaryList != emptyList<Resource<Movie_list>>()){
+                temporaryList.clear()
+                currentPage++
+            }
+            val movieList = Resource.toResource { movieListService.fetchPopularMovie(currentPage) }
+            onSuccess(movieList)
+        } catch (ex: Throwable) {
+            onError(ex)
+        }
+   }
+
+    /*  It will send Movie list when there is liked movies
+    *   if there is no movie in liked
+    *   data will taken from API service
+    *   if everything is go wrong it will execute error function
+    * */
+    suspend fun getMovies(
+            onLikedMovieList: (List<Movie>) -> Unit,
+            onServiceCalled: (Resource<Movie_list>) -> Unit,
+            onMainError: (String?) -> Unit
+    ) = withContext(Dispatchers.IO) {
+        val movieLikedList: MutableList<Movie> = mutableListOf()
+        try {
+            moviesInLikedCross(
+                    onSuccess = {
+                        try {
+                            it.map {
+                                movieLikedList.add( movieDao.getMovie(it.movie_id) )
+                            }
+                            onLikedMovieList(movieLikedList)
+                        } catch (ex: Throwable) {
+                            onMainError(ex.localizedMessage)
+                        }
+                    },
+                    onError = {
+                        launch(Dispatchers.IO) {
+                            getMovieFromPopular(
+                                onSuccess = {
+                                    onServiceCalled(it)
+                                },
+                                onError = {
+                                    onMainError(it.localizedMessage)
+                                }
+                            )
+                        }
+                    }
+            )
+        } catch (ex: Throwable){
+            onMainError(ex.localizedMessage)
+        }
+    }
+
+
     suspend fun getRecommendedMovies(movie_id: Int, page: Int = 1): Resource<Movie_list> {
         return Resource.toResource { movieListService.fetchRecommendationMovie(movie_id, page) }
     }
 
-    fun addMovieToDatabase(movie: Movie, library_name: String) {
+    suspend fun addMovieToDatabase(movie: Movie, library_name: String) {
         movieDao.insertMovieList(listOf(movie))
         croffRefDao.insert(MovieLibraryCrossRef(library_name, movie.movie_id))
     }
--- app/src/main/java/com/example/givemeamovie/repository/MovieWatchListRepository.kt
+++ app/src/main/java/com/example/givemeamovie/repository/MovieWatchListRepository.kt
@@ -6,6 +6,9 @@ import com.example.givemeamovie.data.local.MovieLibraryWithMoviesDao
 import com.example.givemeamovie.model.entity.Movie
 import com.example.givemeamovie.model.entity.MovieLibrary
 import com.example.givemeamovie.model.entity.MovieLibraryCrossRef
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.flow
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 class MovieWatchListRepository  @Inject constructor(
@@ -14,27 +17,28 @@ class MovieWatchListRepository  @Inject constructor(
         private val movieDao: MovieDao
 ): Repository {
 
-    fun insertNewLibrary(library_name: String) {
+    fun getAvailabeleLibraries() = flow {
+        val movieLibraries = movieLibraryDao.getAvailableLibraries()
+        emit(movieLibraries)
+    }.flowOn(Dispatchers.IO)
+
+    suspend fun insertNewLibrary(library_name: String) {
         movieLibraryDao.addNewMovieLibrary(MovieLibrary(library_name))
     }
 
-    fun insertMovie(movie: Movie) {
+    suspend fun insertMovie(movie: Movie) {
         movieDao.insertMovieList(listOf(movie))
     }
 
-    fun insertMovieToLibrary(movie: Movie, library_name: String) {
+    suspend fun insertMovieToLibrary(movie: Movie, library_name: String) {
         movieLibraryWithMoviesDao.insert(MovieLibraryCrossRef(library_name, movie.movie_id))
     }
 
-    fun getAvailabeleLibraries(): List<MovieLibrary> {
-        return movieLibraryDao.getAvailableLibraries()
-    }
-
-    fun deleteMovie(movie: Movie) {
+    suspend fun deleteMovie(movie: Movie) {
         movieDao.deleteMovie(movie)
     }
 
-    fun deleteMovieFromLibrary(movie: Movie, library_name: String) {
+    suspend fun deleteMovieFromLibrary(movie: Movie, library_name: String) {
         movieLibraryWithMoviesDao.deleteMovie(MovieLibraryCrossRef(library_name, movie.movie_id))
     }
 }
